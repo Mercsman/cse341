7287e40dba0ec94be91a8c77c9f9ec0b8799be6f
mercsman <tropicalxfury@gmail.com>
2024-03-09 14:59:57 -0800

    routes, swagger

---
+++ swagger.js
@@ -0,0 +1,20 @@
+const swaggerAutogen = require('swagger-autogen')();
+const doc = {
+    info: {
+        title: 'Contacts API',
+        description: 'Contacts API',
+    },
+    host: 'localhost:3000',
+    schemes: ['http', 'https'],
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+
+// generate swagger.json
+swaggerAutogen(outputFile, endpointsFiles, doc);
+
+// a way to run server after it gets generated
+// swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
+//     await import('./index.js')
+// });
