21e655775be0ab761e2659997889fef269768279
mercsman <tropicalxfury@gmail.com>
2024-03-22 23:16:30 -0700

    .

--- swagger.js
+++ swagger.js
@@ -4,7 +4,7 @@ const doc = {
         title: 'Project 2',
         description: 'Project 2',
     },
-    host: 'localhost:3000',
+    host: 'https://project1-ck8y.onrender.com/',
     schemes: ['http', 'https'],
 };
 
